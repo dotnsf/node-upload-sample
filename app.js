d9ffea0088dfadfec27076b50b0d547b922ce33f
K.Kimura <dotnsf@gmail.com>
2017-05-23 18:05:08 +0900

    image file list

--- app.js
+++ app.js
@@ -10,7 +10,36 @@ var express = require( 'express' ),
 var crypto = require( 'crypto' );
 
 app.use( multer( { dest: './uploads/' } ).single( 'img' ) );
-app.use( express.static( __dirname + '/public' ) );
+app.use( express.static( __dirname + '/images' ) );
+
+app.get( '/', function( req, res ){
+  var html = '<html>'
+    + '<head>'
+    + '<title>node-upload</title>'
+    + '</head>'
+    + '<body>'
+    + '<form method="POST" action="./upload" enctype="multipart/form-data">'
+    + '<input type="file" name="img"/><input type="submit" value="upload"/>'
+    + '</form>'
+    + '<hr/>'
+    + '<ul>';
+
+  var files = fs.readdirSync( './images/' );
+  for( file in files ){
+    var filename = files[file];
+    if( filename != '.gitkeep' ){
+      var li = '<li><a target="_blank" href="./' + filename + '">' + filename + '</a></li>';
+      html += li;
+    }
+  }
+
+  html += '</ul>'
+    + '</body>'
+    + '</html>';
+
+  res.writeHead( 200, { 'Content-Type': 'text/html' } );
+  res.end( html );
+});
 
 app.post( '/upload', function( req, res ){
   //console.log( req.file );
